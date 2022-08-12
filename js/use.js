070c655791259010c05c19278ce567624fab9654
ntj <ntowbinj@gmail.com>
2022-08-12 23:48:21 +0000

    with mesh

--- js/use.js
+++ js/use.js
@@ -7,10 +7,9 @@ const Pictures = function() {
 
 
     //const base = Mat.scale([[0, 0, 0], [1, 0, 0], [Math.cos(Math.PI / 3), Math.sin(Math.PI / 3), 0]], 1);
-    //const cb = icosahedron();
-    const cb = icosahedron();
-    const triangles = [];
-    var id = 0;
+    //const ico = icosahedron();
+    const ico = icosahedronMesh({});
+    const icosas = [];
     for (var k = -200; k <= 10; k++) {
         for (var i = -40; i <= 40; i++) {
             for (var j = -40; j <= 40; j++) {
@@ -22,29 +21,22 @@ const Pictures = function() {
                             Mat.counterClockYZ(randAngle())
                         )
                     );
-                    const theCube = Mat.translateRecursive(
+                    const vertsTrans = Mat.translateRecursive(
                         Mat.scaleRecursive(
-                            Mat.prodRecursive(cb, randRot),
+                            Mat.prodRecursive(ico.verteces.vertMatrix, randRot),
                             sampleExp(1, .001 + Math.random())
                         ),
                         [i, j, k]
                     );
                     const color = tinycolor.random();
-                    for (var c = 0; c < theCube.length; c++) {
-                        id += 1;
-                        triangles.push(
-                            triang(
-                                theCube[c],
-                                {color: color},
-                                id
-                            )
-                        );
-                    }
+                    const newIcosa = icosahedronMesh({color: color});
+                    newIcosa.verteces = Verteces(vertsTrans);
+                    icosas.push(newIcosa);
                 }
             }
         }
     }
-    shuffle(triangles);
+    shuffle(icosas);
     return {
 
         inputs: [
@@ -55,7 +47,8 @@ const Pictures = function() {
 
         draw: function() {
             drawBackground();
-            const allTriangs = logical.getAllTriangles(triangles);
+            //const allTriangs = logical.getAllTriangles(triangles);
+            const allTriangs = logical.getAllTrianglesMeshes(icosas);
             physical.draw(allTriangs);
             logical.drawLineList(getAxes3d(5).map(v => Mat.addVec(v, [0, 0, 0])), {color: 'white'});
         }
