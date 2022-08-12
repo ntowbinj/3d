77e9f8c303fcfd7a3525166e1f3a7bb43aa5cf3d
ntj <ntowbinj@gmail.com>
2022-08-12 23:48:04 +0000

    record video thing

--- js/app.js
+++ js/app.js
@@ -333,15 +333,6 @@ const Mat = {
     }
 }
 
-
-const main = function() {
-    G.config = euler;
-    setUpCanvas();
-    init();
-    update();
-    draw();
-}
-
 const euler = {
     update: function() {
         const n = 1.0 * Math.round(100 * S.n);
@@ -412,6 +403,40 @@ const config1 = {
         logical.drawVecOrig(Mat.trans(S.C)[1]);
     }
 
+}
+
+const main = function() {
+    G.config = euler;
+    setUpCanvas();
+    init();
+    update();
+    draw();
+    //startRecording();
+}
+
+function startRecording() {
+    const chunks = []; // here we will store our recorded media chunks (Blobs)
+    const stream = canvas.captureStream(); // grab our canvas MediaStream
+    const rec = new MediaRecorder(stream); // init the recorder
+    // every time the recorder has new data, we will store it in our array
+    rec.ondataavailable = e => chunks.push(e.data);
+    // only when the recorder stops, we construct a complete Blob from all the chunks
+    rec.onstop = e => exportVid(new Blob(chunks, {type: 'video/webm'}));
+
+    rec.start();
+    setTimeout(()=>rec.stop(), 8000); // stop recording in 3s
+}
+
+function exportVid(blob) {
+    const vid = document.createElement('video');
+    vid.src = URL.createObjectURL(blob);
+    vid.controls = true;
+    document.body.appendChild(vid);
+    const a = document.createElement('a');
+    a.download = 'myvid.webm';
+    a.href = vid.src;
+    a.textContent = 'download the video';
+    document.body.appendChild(a);
 }
 
 window.addEventListener('load', main);
