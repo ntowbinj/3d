0776b9a6f21a8110b2162e307e1b3369e70e98d7
ntj <ntowbinj@gmail.com>
2022-08-12 23:48:21 +0000

    icosa animation

--- js/use.js
+++ js/use.js
@@ -8,14 +8,14 @@ const Pictures = function() {
 
     //const base = Mat.scale([[0, 0, 0], [1, 0, 0], [Math.cos(Math.PI / 3), Math.sin(Math.PI / 3), 0]], 1);
     //const ico = icosahedron();
-    Math.seedrandom('asdf');
+    Math.seedrandom('2');
     const ico = icosahedronMesh({});
     const icosas = [];
     var id = 0;
-    for (var k = -200; k <= 10; k++) {
-        for (var i = -40; i <= 40; i++) {
-            for (var j = -40; j <= 40; j++) {
-                if (Math.random() > 0.99975) {
+    for (var k = -400; k <= 10; k++) {
+        for (var i = -50; i <= 50; i++) {
+            for (var j = -50; j <= 50; j++) {
+                if (Math.random() > 0.99995) {
                     const randRot = Mat.prod(
                         Mat.counterClockXY(randAngle()),
                         Mat.prod(
@@ -26,7 +26,7 @@ const Pictures = function() {
                     const vertsTrans = Mat.translateRecursive(
                         Mat.scaleRecursive(
                             Mat.prodRecursive(ico.verteces.vertMatrix, randRot),
-                            sampleExp(1, .001 + Math.random())
+                            0.5 * samplePareto(.001 + Math.random())
                         ),
                         [i, j, k]
                     );
@@ -38,6 +38,28 @@ const Pictures = function() {
             }
         }
     }
+
+    const randRot = Mat.prod(
+        Mat.counterClockXY(randAngle()),
+        Mat.prod(
+            Mat.counterClockXZ(randAngle()),
+            Mat.counterClockYZ(randAngle())
+        )
+    );
+    const bigOne = icosahedronMesh({color: tinycolor.random()});
+    const vertsTrans = Mat.translateRecursive(
+        Mat.scaleRecursive(
+            Mat.prodRecursive(ico.verteces.vertMatrix, randRot),
+            700
+        ),
+        [400, -3000, -500]
+    )
+    bigOne.verteces = Verteces(vertsTrans);
+    icosas.push(bigOne);
+
+
+
+
     shuffle(icosas);
     return {
 
